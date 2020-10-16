38391c35f54fc9c773a0367af728ae4525487774
jyoti <jjyoti@libertyglobal.com>
2020-10-16 02:20:32 +0200

    wifi: add wifi_getWpsStatus()
    WebUI related backend implementation in wifi subsystem (required by
    X_LGI-COM_WpsStatus).
    
    Signed-off-by: jyoti <jjyoti@libertyglobal.com>

--- wifi_hal.h
+++ wifi_hal.h
@@ -6898,6 +6898,28 @@ INT wifi_setApWpsButtonPush(INT apIndex);                             // This fu
 */
 INT wifi_cancelApWPS(INT apIndex);                                    // cancels WPS mode for this AP
 
+/* wifi_getWpsStatus() function */
+/**
+* @brief Outputs the WPS status of this access point in output_string.
+*
+* @param[in]  apIndex       Access Point index
+* @param[out] output_string   WPS Status, to be returned(Expected values : Success,Failed,In_progress)
+*
+* Device.WiFi.AccessPoint.{x}.WPS.X_LGI-COM_WpsStatus
+*
+* @return The status of the operation
+* @retval RETURN_OK if successful
+* @retval RETURN_ERR if any error is detected
+*
+* @execution Synchronous
+* @sideeffect None
+*
+* @note This function must not suspend and must not invoke any blocking system
+* calls. It should probably just send a message to a driver event handler task.
+*
+*/
+INT wifi_getWpsStatus(INT apIndex, CHAR *output_string);     // Get the WPS status
+
 /* wifi_getApManagementFramePowerControl() function */
 /**
 * @brief Get the ApManagementFramePowerControl
