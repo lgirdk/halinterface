06233f4f1df05ba3eee3bab195f54310f026a45c
Ajay Vishnu Bhat <abhat@libertyglobal.com>
2021-03-02 14:15:48 +0100

    platform: add platform_hal_GetCmMacAddress()
    Signed-off-by: Ajay Vishnu Bhat <abhat@libertyglobal.com>

--- platform_hal.h
+++ platform_hal.h
@@ -1004,6 +1004,7 @@ INT platform_hal_SetLowPowerModeState(PPSM_STATE pState);
 
 INT platform_hal_getCMTSMac(CHAR *pValue);
 
+int platform_hal_GetCmMacAddress (char *pValue, unsigned int len);
 int platform_hal_getUIDefaultPassword (char *pValue, unsigned int len);
 
 #ifdef __cplusplus
