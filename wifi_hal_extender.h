0a8d275d8b54e1861f4d2ae563a20070d6677049
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2022-02-04 16:20:30 +0100

    wifi: add wifi_getApAclDevicesList() ( Part of OFW-2114 )
    API which can be used to get wifi mac filtering entries mapped with hostname.
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

--- wifi_hal_extender.h
+++ wifi_hal_extender.h
@@ -1599,6 +1599,9 @@ INT wifi_getSoftBlockBlacklistEntries(INT band, ULONG *num, wifi_softblock_mac_t
 INT wifi_getDefaultSsid(INT index, CHAR *default_ssid);
 INT wifi_getDefaultPassword(INT index, CHAR *default_password);
 
+/*Get Mac filter list along with hostname from wifi db*/
+INT wifi_getApAclDevicesList(INT apIndex, CHAR **macArray, CHAR **devArray, UINT *buf_size);
+
 /** @} */  //END OF GROUP WIFI_HAL_APIS
 
 #ifdef __cplusplus
